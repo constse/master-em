a63ed754cdbc9721af31a6a225ef194f3de36141
Constantine Seleznyoff <const.seoff@gmail.com>
2015-03-21 08:40:50 +0600

    Добавлена страница контактов

--- src/Master/SiteBundle/Controller/GeneralController.php
+++ src/Master/SiteBundle/Controller/GeneralController.php
@@ -6,8 +6,17 @@ use Master\SystemBundle\Controller\InitializableController;
 
 class GeneralController extends InitializableController
 {
+    public function contactsAction()
+    {
+        return $this->render('MasterSiteBundle:General:contacts.html.twig', array(
+            'menu_active' => 'contacts'
+        ));
+    }
+
     public function indexAction()
     {
-        return $this->render('MasterSiteBundle:General:index.html.twig');
+        return $this->render('MasterSiteBundle:General:index.html.twig', array(
+            'menu_active' => 'index'
+        ));
     }
 } 
