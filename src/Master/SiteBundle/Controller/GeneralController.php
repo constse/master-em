8724669ddc513051375aa5c0ba9e2f15085986d7
Constantine Seleznyoff <const.seoff@gmail.com>
2015-03-26 00:08:33 +0600

    Добавлены все недостающие страницы основного сайта

--- src/Master/SiteBundle/Controller/GeneralController.php
+++ src/Master/SiteBundle/Controller/GeneralController.php
@@ -6,6 +6,13 @@ use Master\SystemBundle\Controller\InitializableController;
 
 class GeneralController extends InitializableController
 {
+    public function bookingAction()
+    {
+        return $this->render('MasterSiteBundle:General:booking.html.twig', array(
+            'menu_active' => 'booking'
+        ));
+    }
+
     public function contactsAction()
     {
         return $this->render('MasterSiteBundle:General:contacts.html.twig', array(
@@ -13,10 +20,24 @@ class GeneralController extends InitializableController
         ));
     }
 
+    public function flightsAction()
+    {
+        return $this->render('MasterSiteBundle:General:flights.html.twig', array(
+            'menu_active' => 'flights'
+        ));
+    }
+
     public function indexAction()
     {
         return $this->render('MasterSiteBundle:General:index.html.twig', array(
             'menu_active' => 'index'
+        ));
+    }
+
+    public function visaAction()
+    {
+        return $this->render('MasterSiteBundle:General:visa.html.twig', array(
+            'menu_active' => 'visa'
         ));
     }
 } 
