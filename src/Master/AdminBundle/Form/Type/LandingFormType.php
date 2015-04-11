3118825de4f39b4042975fa5ef8308810bc202a4
Constantine Seleznyoff <const.seoff@gmail.com>
2015-04-11 21:33:37 +0600

    Обновление

--- src/Master/AdminBundle/Form/Type/LandingFormType.php
+++ src/Master/AdminBundle/Form/Type/LandingFormType.php
@@ -16,6 +16,9 @@ class LandingFormType extends AbstractEntityFormType
     {
         $builder->add('forIndex', new ImageFormType(), array('label' => 'Изображение для главной', 'required' => false))
             ->add('indexCaption', 'textarea', array('label' => 'Заголовок для слайдера'))
-            ->add('forLanding', new ImageFormType(), array('label' => 'Изображение для лэндинга', 'required' => false));
+            ->add('forLanding', new ImageFormType(), array('label' => 'Изображение для лэндинга', 'required' => false))
+            ->add('landH1', 'textarea', array('label' => 'Заголовок лэндинга уровень 1'))
+            ->add('landH2', 'textarea', array('label' => 'Заголовок лэндинга уровень 2', 'required' => false))
+            ->add('landH3', 'textarea', array('label' => 'Заголовок лэндинга уровень 3', 'required' => false));
     }
 } 
--- src/Master/SystemBundle/Entity/Landing.php
+++ src/Master/SystemBundle/Entity/Landing.php
@@ -38,13 +38,37 @@ class Landing extends AbstractEntity
      */
     protected $indexCaption;
 
+    /**
+     * @var string
+     * @ORM\Column(name = "landh1", type = "text")
+     */
+    protected $landH1;
+
+    /**
+     * @var string
+     * @ORM\Column(name = "landh2", type = "text", nullable = true)
+     */
+    protected $landH2;
+
+    /**
+     * @var string
+     * @ORM\Column(name = "landh3", type = "text", nullable = true)
+     */
+    protected $landH3;
+
     public function getForIndex() { return $this->forIndex; }
     public function getForLanding() { return $this->forLanding; }
     public function getIndex() { return $this->index; }
     public function getIndexCaption() { return $this->indexCaption; }
+    public function getLandH1() { return $this->landH1; }
+    public function getLandH2() { return $this->landH2; }
+    public function getLandH3() { return $this->landH3; }
 
     public function setForIndex(Image $forIndex) { $this->forIndex = $forIndex; return $this; }
     public function setForLanding(Image $forLanding) { $this->forLanding = $forLanding; return $this; }
     public function setIndex($index) { $this->index = $index; return $this; }
     public function setIndexCaption($indexCaption) { $this->indexCaption = $indexCaption; return $this; }
-} 
+    public function setLandH1($h) { $this->landH1 = $h; return $this; }
+    public function setLandH2($h) { $this->landH2 = $h; return $this; }
+    public function setLandH3($h) { $this->landH3 = $h; return $this; }
+}
